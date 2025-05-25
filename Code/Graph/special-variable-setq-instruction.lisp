a7b447c428d8fbd53bf3c718aa2d34066ee60c49
Robert Strandh <robert.strandh@gmail.com>
2025-05-25 13:36:55 +0200

    Turn comment into a documentation string.

--- Code/Graph/special-variable-setq-instruction.lisp
+++ Code/Graph/special-variable-setq-instruction.lisp
@@ -1,10 +1,26 @@
 (cl:in-package #:hirundine-graph)
 
-;;; This instruction has two inputs.  The first input is a
-;;; dynamic-environment object.  The second input is the value to
-;;; assign.  This instruction has no outputs.
-
 (defclass special-variable-setq-instruction (instruction)
   ((%variable-name
     :initarg :variable-name
     :reader variable-name)))
+
+(setf (documentation 'special-variable-setq-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               special-variable-setq-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has two inputs and no~@
+               outputs.  The first input is a register containing~@
+               a dynaic-environment object.  The second input is~@
+               a register or a literal.
+               ~@
+               An instruction of this type has a single successor.~@
+               It also has an additional slot containing the name~@
+               of a special variable.
+               ~@
+               The result of executing an instruction of this type~@
+               is that the value of the special variable named in~@
+               the additional slot in the dynamic environment of~@
+               the first input, is changed to be the value of the~@
+               second input."))
