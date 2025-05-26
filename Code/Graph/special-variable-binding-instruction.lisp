41b269d768fb1ec661bd74913926602ac91467aa
Robert Strandh <robert.strandh@gmail.com>
2025-05-26 14:23:44 +0200

    Convert comment into a documentation string.

--- Code/Graph/special-variable-binding-instruction.lisp
+++ Code/Graph/special-variable-binding-instruction.lisp
@@ -1,15 +1,27 @@
 (cl:in-package #:hirundine-graph)
 
-;;; This instruction has two or three inputs.  The first input is a
-;;; dynamic-environment object representing the current dynamic
-;;; environment.  The second input is a symbol naming the variable to
-;;; bind.  If present, the third input is a value to give to the
-;;; variable in the new binding.  If the third input is absent, the
-;;; new binding has no initial value.
-;;;
-;;; This instruction has a single output which is a
-;;; dynamic-environment object that is like the one in the input
-;;; except that a new entry representing the binding has been added.
-
 (defclass special-variable-binding-instruction (instruction)
   ())
+
+(setf (documentation 'special-variable-binding-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               special-variable-binding-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has two or three inputs~@
+               and one output.  The first input is a register containing~@
+               a dynamic-environment object.  The second input is~@
+               a register or a literal.  If present, the third input~@
+               is a register or a literal.  The output is a register~@
+               that contains a dynamic-environment object.
+               ~@
+               An instruction of this type has a single successor.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that a new binding is created for the special~@
+               variable named by the second input.  The output~@
+               dynamic-environment object contains that new binding.~@
+               If the third input is present, then its value is the~@
+               value of the new binding.  If the third input is not~@
+               present, the new binding will have no values, as is~@
+               possible if the new binding was created using PROGV."))
